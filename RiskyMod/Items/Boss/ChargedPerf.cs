ede092c1fddcb97941421941b5108766f8d2c5bb
Moffein <22742925+Moffein@users.noreply.github.com>
2021-10-23 01:57:08 -0700

    Gave Molten/Charged Perforator the same scaling as AtGs/Bands

--- RiskyMod/Items/Boss/ChargedPerf.cs
+++ RiskyMod/Items/Boss/ChargedPerf.cs
@@ -1,6 +1,8 @@
 ﻿using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using R2API;
 using RoR2;
+using System;
 
 namespace Risky_Mod.Items.Boss
 {
@@ -9,7 +11,9 @@ namespace Risky_Mod.Items.Boss
         public static bool enabled = true;
         public static void Modify()
         {
-            if (!enabled || !RiskyMod.disableProcChains) return;
+            if (!enabled) return;
+
+            LanguageAPI.Add("ITEM_FIREBALLSONHIT_DESC", "<style=cIsDamage>10%</style> chance on hit to call forth <style=cIsDamage>3 magma balls</style> from an enemy, dealing <style=cIsDamage>300%</style> <style=cStack>(+180% per stack)</style> damage each.");
 
             //Remove Vanilla Effect
             IL.RoR2.GlobalEventManager.OnHitEnemy += (il) =>
@@ -18,11 +22,26 @@ namespace Risky_Mod.Items.Boss
                 c.GotoNext(
                      x => x.MatchLdsfld(typeof(RoR2Content.Items), "LightningStrikeOnHit")
                     );
+
                 c.GotoNext(
-                    x => x.MatchStfld<RoR2.Orbs.GenericDamageOrb>("procCoefficient")
+                    x => x.MatchLdfld<DamageInfo>("damage")
                     );
-                c.Index--;
-                c.Next.Operand = 0f;
+                c.Index += 3;
+                c.Next.Operand = 3f;
+                c.Index += 4;
+                c.EmitDelegate<Func<float, float>>((damageCoefficient) =>
+                {
+                    return damageCoefficient + 2f;
+                });
+
+                if (RiskyMod.disableProcChains)
+                {
+                    c.GotoNext(
+                        x => x.MatchStfld<RoR2.Orbs.GenericDamageOrb>("procCoefficient")
+                        );
+                    c.Index--;
+                    c.Next.Operand = 0f;
+                }
             };
 
             //Effect handled in SharedHooks.OnHitEnemy
--- RiskyMod/Items/Boss/MoltenPerf.cs
+++ RiskyMod/Items/Boss/MoltenPerf.cs
@@ -15,29 +15,49 @@ namespace Risky_Mod.Items.Boss
 
         public static void Modify()
         {
-            if (!enabled || !RiskyMod.disableProcChains) return;
+            if (!enabled) return;
+
+            LanguageAPI.Add("ITEM_LIGHTNINGSTRIKEONHIT_DESC", "<style=cIsDamage>10%</style> chance on hit to down a lightning strike, dealing <style=cIsDamage>500%</style> <style=cStack>(+300% per stack)</style> damage.");
 
             //Remove Vanilla Effect
             IL.RoR2.GlobalEventManager.OnHitEnemy += (il) =>
             {
                 ILCursor c = new ILCursor(il);
-                /*c.GotoNext(
-                     x => x.MatchLdsfld(typeof(RoR2Content.Items), "FireballsOnHit")
-                    );*/
                 c.GotoNext(
-                    x => x.MatchLdstr("Prefabs/Projectiles/FireMeatBall")
+                     x => x.MatchLdsfld(typeof(RoR2Content.Items), "FireballsOnHit")
                     );
-                c.Index += 2;
-                c.EmitDelegate<Func<GameObject, GameObject>>((oldPrefab) =>
+                c.GotoNext(
+                     x => x.MatchLdfld<DamageInfo>("damage")
+                    );
+                c.Index -= 6;
+                c.Next.Operand = 1.8f;
+                c.Index += 4;
+                c.EmitDelegate<Func<float, float>>((damageCoefficient) =>
                 {
-                    return MoltenPerf.meatballPrefab;
+                    return damageCoefficient + 1.2f;
                 });
+
+
+                if (RiskyMod.disableProcChains)
+                {
+                    c.GotoNext(
+                        x => x.MatchLdstr("Prefabs/Projectiles/FireMeatBall")
+                        );
+                    c.Index += 2;
+                    c.EmitDelegate<Func<GameObject, GameObject>>((oldPrefab) =>
+                    {
+                        return MoltenPerf.meatballPrefab;
+                    });
+                }
             };
 
-            meatballPrefab = Resources.Load<GameObject>("Prefabs/Projectiles/FireMeatBall").InstantiateClone("RiskyItemTweaks_Perforator", true);
-            ProjectileImpactExplosion pie = meatballPrefab.GetComponent<ProjectileImpactExplosion>();
-            pie.blastProcCoefficient = 0f;
-            ProjectileAPI.Add(meatballPrefab);
+            if (RiskyMod.disableProcChains)
+            {
+                meatballPrefab = Resources.Load<GameObject>("Prefabs/Projectiles/FireMeatBall").InstantiateClone("RiskyItemTweaks_Perforator", true);
+                ProjectileImpactExplosion pie = meatballPrefab.GetComponent<ProjectileImpactExplosion>();
+                pie.blastProcCoefficient = 0f;
+                ProjectileAPI.Add(meatballPrefab);
+            }
         }
     }
 }
