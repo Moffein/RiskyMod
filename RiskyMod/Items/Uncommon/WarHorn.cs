5e91ce3b8f2f76b90cc8838d9aabf21f411b121e
Moffein <22742925+Moffein@users.noreply.github.com>
2021-11-04 17:17:24 -0700

    Added language tokens for War Horn changes.

--- RiskyMod/Items/Uncommon/WarHorn.cs
+++ RiskyMod/Items/Uncommon/WarHorn.cs
@@ -1,4 +1,6 @@
-﻿namespace RiskyMod.Items.Uncommon
+﻿using R2API;
+
+namespace RiskyMod.Items.Uncommon
 {
     public class WarHorn
     {
@@ -6,7 +8,12 @@
         public WarHorn()
         {
             if (!enabled) return;
-            //Code handled in SharedHooks.GetStatCoefficients
+
+            LanguageAPI.Add("ITEM_ENERGIZEDONEQUIPMENTUSE_PICKUP", "Activating your Equipment gives you a burst of movement speed and attack speed.");
+            LanguageAPI.Add("ITEM_ENERGIZEDONEQUIPMENTUSE_DESC",
+                "Activating your Equipment gives you <style=cIsUtility>+50% movement speed</style> and <style=cIsDamage>+70% attack speed</style> for <style=cIsDamage>8s</style> <style=cStack>(+4s per stack)</style>.");
+
+            //Buff handled in SharedHooks.GetStatCoefficients
         }
     }
 }
