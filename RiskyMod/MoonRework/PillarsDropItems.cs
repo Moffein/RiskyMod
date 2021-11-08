f3ce765fa1417819b4079ab33b55e2d04629ac38
Moffein <22742925+Moffein@users.noreply.github.com>
2021-11-08 11:14:12 -0800

    Tweaked moon pillar drops to behave more like teleporter drops.

--- RiskyMod/MoonRework/PillarsDropItems.cs
+++ RiskyMod/MoonRework/PillarsDropItems.cs
@@ -12,10 +12,10 @@ namespace RiskyMod.MoonRework
         public static bool enabled = true;
         private static Vector3 rewardPositionOffset = new Vector3(0f, 3f, 0f);
 
-        public static float whiteChance = 40f;
-        public static float greenChance = 30f;
+        public static float whiteChance = 50f;
+        public static float greenChance = 40f;
         public static float redChance = 10f;
-        public static float yellowChance = 20f;
+        public static BossGroup teleBossGroup = Resources.Load<BossGroup>("prefabs/networkedobjects/teleporters/Teleporter1");
 
         public PillarsDropItems()
         {
@@ -37,26 +37,33 @@ namespace RiskyMod.MoonRework
                             Vector3 vector = Quaternion.AngleAxis((float)UnityEngine.Random.Range(0, 360), Vector3.up) * (Vector3.up * 40f + Vector3.forward * 5f);
                             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
 
-                            //Randomly convert pearls into shiny pearls when dropping so that it doesn't drop a ton of them at once
                             PickupIndex pearlIndex = PickupCatalog.FindPickupIndex(RoR2Content.Items.Pearl.itemIndex);
                             PickupIndex shinyPearlIndex = PickupCatalog.FindPickupIndex(RoR2Content.Items.ShinyPearl.itemIndex);
-                            bool randomizePearl = false;
-                            if (pickupIndex == pearlIndex)
-                            {
-                                if (shinyPearlIndex != PickupIndex.none && Run.instance.availableBossDropList.Contains(shinyPearlIndex))
-                                {
-                                    randomizePearl = true;
-                                }
-                            }
+
+                            bool pearlAvailable = pearlIndex != PickupIndex.none && Run.instance.IsItemAvailable(RoR2Content.Items.Pearl.itemIndex);
+                            bool shinyPearlAvailable = shinyPearlIndex != PickupIndex.none && Run.instance.IsItemAvailable(RoR2Content.Items.ShinyPearl.itemIndex);
 
                             int k = 0;
                             while (k < num)
                             {
-                                if (randomizePearl)
+                                bool overwritePickup = false;
+                                PickupIndex pickupOverwrite = PickupIndex.none;
+                                if ((pearlAvailable || shinyPearlAvailable) && teleBossGroup.bossDropChance > 0f && Run.instance.treasureRng.RangeFloat(0f, 100f) <= teleBossGroup.bossDropChance)
                                 {
-                                    pickupIndex = (Run.instance.bossRewardRng.RangeInt(0, 5) != 0) ? pearlIndex : shinyPearlIndex;
+                                    if (pearlAvailable)
+                                    {
+                                        pickupOverwrite = pearlIndex;
+                                        if (shinyPearlAvailable && Run.instance.treasureRng.RangeFloat(0f, 100f) <= 20f)
+                                        {
+                                            pickupOverwrite = shinyPearlIndex;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        pickupOverwrite = shinyPearlIndex;
+                                    }
                                 }
-                                PickupDropletController.CreatePickupDroplet(pickupIndex, holdoutZone.transform.position + rewardPositionOffset, vector);
+                                PickupDropletController.CreatePickupDroplet(overwritePickup ? pickupOverwrite : pickupIndex, holdoutZone.transform.position + rewardPositionOffset, vector);
                                 k++;
                                 vector = rotation * vector;
                             }
@@ -66,79 +73,35 @@ namespace RiskyMod.MoonRework
             };
         }
 
+        //Yellow Chance is handled after selecting item
         private static PickupIndex SelectItem()
         {
             List<PickupIndex> list;
-            Xoroshiro128Plus treasureRng = Run.instance.bossRewardRng;
+            Xoroshiro128Plus bossRewardRng = Run.instance.bossRewardRng;
             PickupIndex selectedPickup = PickupIndex.none;
 
-            float total = whiteChance + greenChance + redChance + yellowChance;
+            float total = whiteChance + greenChance + redChance;
 
-            if (treasureRng.RangeFloat(0f, total) <= whiteChance)//drop white
+            if (bossRewardRng.RangeFloat(0f, total) <= whiteChance)//drop white
             {
                 list = Run.instance.availableTier1DropList;
             }
             else
             {
                 total -= whiteChance;
-                if (treasureRng.RangeFloat(0f, total) <= greenChance)//drop green
+                if (bossRewardRng.RangeFloat(0f, total) <= greenChance)//drop green
                 {
                     list = Run.instance.availableTier2DropList;
                 }
                 else
                 {
                     total -= greenChance;
-                    if (treasureRng.RangeFloat(0f, total) <= redChance)//drop red
-                    {
-                        list = Run.instance.availableTier3DropList;
-                    }
-                    else
-                    {
-                        //There's probably a better way of doing this.
-                        PickupIndex pearlIndex;
-                        PickupIndex shinyPearlIndex;
-
-                        list = new List<PickupIndex>();
-                        pearlIndex = PickupCatalog.FindPickupIndex(RoR2Content.Items.Pearl.itemIndex);
-                        shinyPearlIndex = PickupCatalog.FindPickupIndex(RoR2Content.Items.ShinyPearl.itemIndex);
-
-                        if (pearlIndex != PickupIndex.none && Run.instance.availableBossDropList.Contains(pearlIndex))
-                        {
-                            list.Add(pearlIndex);
-                        }
-                        else //Drop only Shiny Pearls if regular pearls aren't available for some reason.
-                        {
-                            if (shinyPearlIndex != PickupIndex.none && Run.instance.availableBossDropList.Contains(shinyPearlIndex))
-                            {
-                                list.Add(shinyPearlIndex);
-                            }
-                        }
-                        /*if (shinyPearlIndex != PickupIndex.none && Run.instance.availableBossDropList.Contains(shinyPearlIndex))
-                        {
-                            list.Add(shinyPearlIndex);
-                        }*/
-                        if (list.Count <= 0)
-                        {
-                            list = Run.instance.availableTier2DropList;
-                        }
-                        /*else
-                        {
-                            //Random chance moved to when the pillar is dropping items to prevent them from dropping 10 shiny pearls at once.
-                            if (pearlIndex != PickupIndex.none && shinyPearlIndex != PickupIndex.none)
-                            {
-                                //Shiny pearl is locked behind 20% random chance.
-                                if (treasureRng.RangeInt(0, 5) != 0)
-                                {
-                                    list.Remove(shinyPearlIndex);
-                                }
-                            }
-                        }*/
-                    }
+                    list = Run.instance.availableTier3DropList;
                 }
             }
             if (list.Count > 0)
             {
-                selectedPickup = treasureRng.NextElementUniform<PickupIndex>(list);
+                selectedPickup = bossRewardRng.NextElementUniform<PickupIndex>(list);
             }
             return selectedPickup;
         }
