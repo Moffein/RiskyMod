75af637cb7c82ca96530aa88761126e62022d31b
Moffein <22742925+Moffein@users.noreply.github.com>
2021-11-18 19:37:14 -0700

    All drones get shields now.

--- RiskyMod/Drones/DroneScaling.cs
+++ RiskyMod/Drones/DroneScaling.cs
@@ -42,38 +42,42 @@ namespace RiskyMod.Drones
 
             cb.baseRegen = cb.baseMaxHealth / 25f;  //Drones take 20s to regen to full
 
+            bool noShield = false;
+
             //Specific changes
             switch (cb.name)
             {
                 case "MegaDroneBody": //If I'm gonna pay the price of a legendary chest to buy a drone, it better be worth it.
                     cb.bodyFlags |= CharacterBody.BodyFlags.OverheatImmune;
                     cb.baseMaxHealth *= 1.5f;
-                    cb.baseMaxShield = cb.baseMaxHealth * 0.12f;
                     break;
                 case "SquidTurretBody":
                     cb.baseMaxHealth = 720f;
                     break;
                 case "Turret1Body": //Shield seems to be enough to put them in a good spot.
                     cb.baseMaxHealth *= 1.5f;
-                    cb.baseMaxShield = cb.baseMaxHealth * 0.12f;
                     break;
-                case "FlameDroneBody": //These seem to die faster than other drones?
+                /*case "FlameDroneBody": //These seem to die faster than other drones?
                     //cb.baseRegen = cb.baseMaxHealth / 20f;
                     cb.baseMaxHealth *= 1.5f;
-                    break;
+                    break;*/
                 case "BeetleGuardAllyBody":
-                    cb.baseMaxHealth *= 1.5f;
                     cb.levelArmor -= 1f;    //Queens Gland Guards get no armor bonus.
+                    noShield = true;
                     break;
                 default:
                     break;
             }
 
             //This makes their performance stay the same on every stage. (Everything's HP increases 30% per level, so damage and regen increase matches that.)
+            if (!noShield)
+            {
+                cb.baseMaxShield += cb.baseMaxHealth * 0.1f;
+                cb.levelMaxShield = cb.baseMaxShield * 0.3f;
+            }
             cb.levelRegen = cb.baseRegen * 0.3f;
             cb.levelDamage = cb.baseDamage * 0.3f;
             cb.levelArmor += 1f;    //Drones need bonus armor because of increasing enemycounts and elite counts, otherwise they end up dying really quickly.
-            cb.levelMaxShield = cb.baseMaxShield * 0.3f;
             cb.levelMaxHealth = cb.baseMaxHealth * 0.3f;
         }
 
--- RiskyMod/RiskyMod.cs
+++ RiskyMod/RiskyMod.cs
@@ -41,7 +41,7 @@ namespace RiskyMod
     [BepInDependency("com.ThinkInvisible.Hypercrit", BepInDependency.DependencyFlags.SoftDependency)]
 
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.RiskyLives.RiskyMod", "RiskyMod Beta", "0.2.2")]
+    [BepInPlugin("com.RiskyLives.RiskyMod", "RiskyMod Beta", "0.2.3")]
     [R2API.Utils.R2APISubmoduleDependency(nameof(RecalculateStatsAPI), nameof(PrefabAPI),
         nameof(ProjectileAPI), nameof(EffectAPI), nameof(DamageAPI), nameof(BuffAPI))]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
