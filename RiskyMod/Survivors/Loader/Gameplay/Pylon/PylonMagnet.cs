ed51d53e032eb66e6437fc42c42a41dee8a7e0f6
Moffein <22742925+Moffein@users.noreply.github.com>
2022-01-03 02:10:48 -0800

    Pylon Magnetism now pulses. Needs fine-tuning.

--- RiskyMod/Survivors/Loader/Gameplay/Pylon/PylonMagnet.cs
+++ RiskyMod/Survivors/Loader/Gameplay/Pylon/PylonMagnet.cs
@@ -23,11 +23,75 @@ namespace RiskyMod.Survivors.Loader
             RadialForceMassLimited rf = go.AddComponent<RadialForceMassLimited>();
             rf.radius = 25f;
             rf.damping = 0.5f;
-            rf.forceMagnitude = -250f;
+            rf.forceMagnitude = -1000f;  //-250f original
             rf.forceCoefficientAtEdge = 0.5f;
             rf.maxMass = 250f;  //same as alt grapple
 
+            go.AddComponent<PylonPulseController>();
+
             return go;
+        }
+    }
+
+    public class PylonPulseController : MonoBehaviour
+    {
+        public static float initialDelay = 1.65f;
+        public static float timeBetweenShocks = 1f;
+        public static float pulseDuration = 0.4f;
+
+        public RadialForceMassLimited radialForce;
+        public float stopwatch;
+        public float pulseTimer;
+
+        bool hasStarted = false;
+
+        public void Awake()
+        {
+            radialForce = base.GetComponent<RadialForceMassLimited>();
+            radialForce.enabled = false;
+            pulseTimer = 0f;
+            stopwatch = 0f;
+        }
+
+        public void FixedUpdate()
+        {
+            stopwatch += Time.fixedDeltaTime;
+            if (!hasStarted)
+            {
+                if (stopwatch > initialDelay)
+                {
+                    hasStarted = true;
+                    stopwatch = 0f;
+                    StartPulse();
+                }
+            }
+            else
+            {
+                if (pulseTimer > 0f)
+                {
+                    pulseTimer -= Time.fixedDeltaTime;
+                    if (pulseTimer <= 0f)
+                    {
+                        EndPulse();
+                    }
+                }
+                if (stopwatch > timeBetweenShocks)
+                {
+                    stopwatch -= timeBetweenShocks;
+                    StartPulse();
+                }
+            }
+        }
+
+        public void StartPulse()
+        {
+            pulseTimer = pulseDuration;
+            radialForce.enabled = true;
+        }
+
+        public void EndPulse()
+        {
+            radialForce.enabled = false;
         }
     }
 }
