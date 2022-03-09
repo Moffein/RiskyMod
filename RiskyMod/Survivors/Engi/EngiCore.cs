2bd58f3290b607ef2eca92ec888052f7874b1f02
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-09 15:36:44 -0700

    Changed Pressure Mine description to match modded behavior.

--- RiskyMod/Survivors/Engi/EngiCore.cs
+++ RiskyMod/Survivors/Engi/EngiCore.cs
@@ -7,10 +7,27 @@ namespace RiskyMod.Survivors.Engi
     public class EngiCore
     {
         public static bool enabled = true;
+        public static GameObject bodyPrefab = LegacyResourcesAPI.Load<GameObject>("Prefabs/CharacterBodies/EngiBody");
         public EngiCore()
         {
             if (!enabled) return;
+
+            ModifySkills(bodyPrefab.GetComponent<SkillLocator>());
+        }
+
+        private void ModifySkills(SkillLocator sk)
+        {
+            ModifySecondaries(sk);
+            ModifySpecials(sk);
+        }
+
+        private void ModifySecondaries(SkillLocator sk)
+        {
+            sk.secondary.skillFamily.variants[0].skillDef.skillDescriptionToken = "ENGI_SECONDARY_DESCRIPTION_RISKYMOD";
             new PressureMines();
+        }
+        private void ModifySpecials(SkillLocator sk)
+        {
             new TurretChanges();
         }
     }
