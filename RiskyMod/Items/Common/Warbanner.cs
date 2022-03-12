3e599beeca73a1069c7c9479c94a4f5ae6e35354
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-12 03:32:49 -0700

    Trying to get Warbanners to work with Voidling.

--- RiskyMod/Items/Common/Warbanner.cs
+++ RiskyMod/Items/Common/Warbanner.cs
@@ -41,11 +41,36 @@ namespace RiskyMod.Items.Common
                 orig(self);
                 SpawnBanners();
             };
+
+            //Does this work online?
+            /*On.RoR2.VoidRaidGauntletExitController.OnBodyTeleport += (orig, self, body) =>
+            {
+                orig(self, body);
+                if (NetworkServer.active)
+                {
+                    SpawnBanner(body);
+                }
+            };*/
         }
         private static void ModifyItem()
         {
             HG.ArrayUtils.ArrayAppend(ref ItemsCore.changedItemPickups, RoR2Content.Items.WardOnLevel);
             HG.ArrayUtils.ArrayAppend(ref ItemsCore.changedItemDescs, RoR2Content.Items.WardOnLevel);
+        }
+
+        private void SpawnBanner(CharacterBody body)
+        {
+            if (body.inventory && body.teamComponent && body.teamComponent.teamIndex == TeamIndex.Player)
+            {
+                int itemCount = body.inventory.GetItemCount(RoR2Content.Items.WardOnLevel);
+                if (itemCount > 0)
+                {
+                    GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(WarbannerObject, body.transform.position, Quaternion.identity);
+                    gameObject.GetComponent<TeamFilter>().teamIndex = TeamIndex.Player;
+                    gameObject.GetComponent<BuffWard>().Networkradius = 8f + 8f * (float)itemCount;
+                    NetworkServer.Spawn(gameObject);
+                }
+            }
         }
 
         private void SpawnBanners()
