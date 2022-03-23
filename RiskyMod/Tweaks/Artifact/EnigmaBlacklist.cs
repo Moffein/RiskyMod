1a6657c235812940af77a1ba7913cf0652e7240b
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-23 00:00:49 -0700

    Renamed Artifacts folder.

---
+++ RiskyMod/Tweaks/Artifact/EnigmaBlacklist.cs
@@ -0,0 +1,46 @@
+﻿using RoR2;
+using RoR2.Artifacts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyMod.Tweaks.Artifacts
+{
+    public class EnigmaBlacklist
+    {
+        public static bool enabled = true;
+        public static bool blacklistLunar = true;
+
+        public EnigmaBlacklist()
+        {
+            if (!enabled) return;
+
+            On.RoR2.Artifacts.EnigmaArtifactManager.OnRunStartGlobal += (orig, run) =>
+            {
+                orig(run);
+
+                List<EquipmentIndex> toRemove = new List<EquipmentIndex>();
+                foreach (EquipmentIndex i in EnigmaArtifactManager.validEquipment)
+                {
+                    EquipmentDef ed = EquipmentCatalog.GetEquipmentDef(i);
+                    if (ed)
+                    {
+                        if (blacklistLunar && ed.isLunar)
+                        {
+                            toRemove.Add(i);
+                        }
+                    }
+                }
+
+                foreach (EquipmentIndex i in toRemove)
+                {
+                    EnigmaArtifactManager.validEquipment.Remove(i);
+                }
+
+                EnigmaArtifactManager.validEquipment.Remove(RoR2Content.Equipment.Recycle.equipmentIndex);
+                EnigmaArtifactManager.validEquipment.Remove(DLC1Content.Equipment.BossHunter.equipmentIndex);
+                EnigmaArtifactManager.validEquipment.Remove(DLC1Content.Equipment.MultiShopCard.equipmentIndex);
+            };
+        }
+    }
+}
---
+++ RiskyMod/Tweaks/Artifact/VengeancePercentHeal.cs
@@ -0,0 +1,22 @@
+﻿using RoR2;
+
+namespace RiskyMod.Tweaks.Artifacts
+{
+    public class VengeancePercentHeal
+    {
+        public static bool enabled = true;
+        public VengeancePercentHeal()
+        {
+            if (!enabled) return;
+
+            On.RoR2.HealthComponent.HealFraction += (orig, self, fraction, procChainMask) =>
+            {
+                if (self.itemCounts.invadingDoppelganger > 0)
+                {
+                    fraction *= 0.1f;
+                }
+                return orig(self, fraction, procChainMask);
+            };
+        }
+    }
+}
