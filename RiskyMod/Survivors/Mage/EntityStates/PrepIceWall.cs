4d40f968cc66793faf2d486fb5a374430c9b3e71
Moffein <22742925+Moffein@users.noreply.github.com>
2022-04-02 06:47:39 -0700

    Tweaked Snapfreeze indicator behavior.

--- RiskyMod/Survivors/Mage/EntityStates/PrepIceWall.cs
+++ RiskyMod/Survivors/Mage/EntityStates/PrepIceWall.cs
@@ -18,6 +18,13 @@ namespace EntityStates.RiskyMod.Mage.Weapon
 			Util.PlaySound(PrepIceWall.PrepIceWallSoundString, base.gameObject);
 
 			this.areaIndicatorInstance = UnityEngine.Object.Instantiate<GameObject>(PrepIceWall.areaIndicatorPrefab);
+
+			this.areaIndicatorCircleInstance = UnityEngine.Object.Instantiate<GameObject>(PrepIceWall.areaIndicatorCirclePrefab);
+			if (areaIndicatorCircleInstance)
+			{
+				areaIndicatorCircleInstance.transform.localScale = blastRadius * Vector3.one;
+			}
+
 			this.UpdateAreaIndicator();
 		}
 
@@ -50,7 +57,23 @@ namespace EntityStates.RiskyMod.Mage.Weapon
 					this.crosshairOverrideRequest = CrosshairUtils.RequestOverrideForBody(base.characterBody, crosshairPrefab, CrosshairUtils.OverridePriority.Skill);
 				}
 			}
+
+			if (this.areaIndicatorCircleInstance)
+			{
+				Ray aimRay = base.GetAimRay();
+				Vector3 aimPos = aimRay.origin + aimRay.direction * maxDistance;
+				RaycastHit raycastHit = default(RaycastHit);
+				if (Util.CharacterRaycast(base.gameObject, aimRay, out raycastHit, maxDistance, LayerIndex.CommonMasks.bullet, QueryTriggerInteraction.UseGlobal))
+				{
+					aimPos = raycastHit.point;
+				}
+
+				this.areaIndicatorCircleInstance.transform.SetPositionAndRotation(aimPos, Quaternion.identity);
+				areaIndicatorCircleInstance.transform.localScale = blastRadius * Vector3.one;
+			}
+
 			this.areaIndicatorInstance.SetActive(this.goodPlacement);
+			this.areaIndicatorCircleInstance.SetActive(!this.goodPlacement);
 		}
 
 		public override void Update()
@@ -93,6 +116,7 @@ namespace EntityStates.RiskyMod.Mage.Weapon
 				}
 			}
 			EntityState.Destroy(this.areaIndicatorInstance.gameObject);
+			EntityState.Destroy(this.areaIndicatorCircleInstance.gameObject);
 			CrosshairUtils.OverrideRequest overrideRequest = this.crosshairOverrideRequest;
 			if (overrideRequest != null)
 			{
@@ -118,7 +142,7 @@ namespace EntityStates.RiskyMod.Mage.Weapon
 			EffectData effectData = new EffectData();
 			effectData.rotation = Util.QuaternionSafeLookRotation(Vector3.up);
 			effectData.origin = aimPos;
-			effectData.scale = blastRadius * 0.75F;	//VFX is huge for some reason
+			effectData.scale = blastRadius;
 			EffectManager.SpawnEffect(blastEffectPrefab, effectData, true);
 
 			BlastAttack blastAttack = new BlastAttack
@@ -159,11 +183,12 @@ namespace EntityStates.RiskyMod.Mage.Weapon
 
 		public static float baseDuration = 0.5f;
 		public static GameObject areaIndicatorPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mage/FirewallAreaIndicator.prefab").WaitForCompletion();
+		public static GameObject areaIndicatorCirclePrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Huntress/HuntressArrowRainIndicator.prefab").WaitForCompletion();
 		public static GameObject projectilePrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mage/MageIcewallWalkerProjectile.prefab").WaitForCompletion();
 		public static float damageCoefficient = 1f;
 		public static GameObject muzzleflashEffect = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mage/MuzzleflashMageIceLarge.prefab").WaitForCompletion();
 		public static GameObject goodCrosshairPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/UI/SimpleDotCrosshair.prefab").WaitForCompletion();
-		public static GameObject badCrosshairPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/UI/BadCrosshair.prefab").WaitForCompletion();
+		public static GameObject badCrosshairPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/UI/SimpleDotCrosshair.prefab").WaitForCompletion();//Addressables.LoadAssetAsync<GameObject>("RoR2/Base/UI/BadCrosshair.prefab").WaitForCompletion();
 		public static string PrepIceWallSoundString = "Play_mage_shift_start";
 		public static float maxDistance = 600f;
 		public static string fireSoundString = "Play_mage_shift_stop";
@@ -173,7 +198,7 @@ namespace EntityStates.RiskyMod.Mage.Weapon
 		public static GameObject hitEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mage/MageIceExplosion.prefab").WaitForCompletion();
 		public static GameObject blastEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Common/VFX/OmniImpactVFXFrozen.prefab").WaitForCompletion();
 
-		public static float blastRadius = 8f;//Ion Surge is 14f
+		public static float blastRadius = 6f;//Ion Surge is 14f
 		public static float blastJumpRadius = 14f;
 
 		public static float blastForce = 3000f;
@@ -183,6 +208,7 @@ namespace EntityStates.RiskyMod.Mage.Weapon
 		private float stopwatch;
 		private bool goodPlacement;
 		private GameObject areaIndicatorInstance;
+		private GameObject areaIndicatorCircleInstance;
 		private CrosshairUtils.OverrideRequest crosshairOverrideRequest;
 	}
 }
