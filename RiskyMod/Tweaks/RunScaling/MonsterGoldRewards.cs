002c7273ef8c9890dcb53929838ea8bc73ba44aa
Moffein <22742925+Moffein@users.noreply.github.com>
2022-04-21 16:34:25 -0700

    Gold rewards are now adjusted on spawn instead of on death.

--- RiskyMod/Tweaks/RunScaling/MonsterGoldRewards.cs
+++ RiskyMod/Tweaks/RunScaling/MonsterGoldRewards.cs
@@ -20,18 +20,23 @@ namespace RiskyMod.Tweaks.RunScaling
 				orig(self);
 			};
 
-
-			On.RoR2.DeathRewards.OnKilledServer += (orig, self, damageReport) =>
+			On.RoR2.CharacterBody.Start += (orig, self) =>
 			{
-				if (Run.instance.gameModeIndex != RiskyMod.simulacrumIndex)
-				{
-					float chestRatio = scaleToChests ? stageChestCost / (float)Run.instance.GetDifficultyScaledCost(25) : 1f;
-					float inflationRatio = scaleToInflation ? 1.4f / (1f + 0.4f * Run.instance.difficultyCoefficient) : 1f;	//Couldn't find actual code, but wiki claims Combat Director spawning crerdits gets multiplied by this.
+				orig(self);
 
-					int goldRewardRaw = (int)Mathf.Max(Mathf.Round(self.goldReward * chestRatio * inflationRatio), 1f);
-					self.goldReward = (uint)goldRewardRaw;
+				if (Run.instance.gameModeIndex != RiskyMod.simulacrumIndex)
+                {
+					DeathRewards dw = self.GetComponent<DeathRewards>();
+					if (dw)
+					{
+						Debug.Log(dw.goldReward);
+						float chestRatio = scaleToChests ? stageChestCost / (float)Run.instance.GetDifficultyScaledCost(25) : 1f;
+						float inflationRatio = scaleToInflation ? 1.4f / (1f + 0.4f * Run.instance.difficultyCoefficient) : 1f; //Couldn't find actual code, but wiki claims Combat Director spawning crerdits gets multiplied by this.
+
+						int goldRewardRaw = (int)Mathf.Max(Mathf.Round(dw.goldReward * chestRatio * inflationRatio), 1f);
+						dw.goldReward = (uint)goldRewardRaw;
+					}
 				}
-				orig(self, damageReport);
 			};
 		}
     }
