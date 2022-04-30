97a921869289f89a48168ac4b7a74ba522dc97c1
Moffein <22742925+Moffein@users.noreply.github.com>
2022-04-30 15:16:24 -0700

    Fixed NetworkedTimedBuffs incompatibility.

--- RiskyMod/Items/Lunar/Transcendence.cs
+++ RiskyMod/Items/Lunar/Transcendence.cs
@@ -16,13 +16,10 @@ namespace RiskyMod.Items.Lunar
             IL.RoR2.CharacterBody.FixedUpdate += (il) =>
             {
                 ILCursor c = new ILCursor(il);
-                c.GotoNext(
-                     x => x.MatchCall<CharacterBody>("UpdateBuffs"),
-                     x => x.MatchLdarg(0),
+                c.GotoNext(MoveType.After,
                      x => x.MatchLdfld<CharacterBody>("outOfDangerStopwatch"), //Isn't there any other way to increase shield recharge delay? This is messing with other items such as red whip, slug, or even the new reworked OSP. -anreol
                      x => x.MatchLdcR4(7f)
                     );
-                c.Index += 4;
                 c.Emit(OpCodes.Ldarg_0);
                 c.EmitDelegate<Func<float, CharacterBody, float>>((outOfDangerDelay, self) =>
                 {
