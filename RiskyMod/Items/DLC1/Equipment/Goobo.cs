f6852eb387b65c765ce4b87eab72089a69e4aa22
Moffein <22742925+Moffein@users.noreply.github.com>
2022-05-02 05:01:08 -0700

    Goobo

--- RiskyMod/Items/DLC1/Equipment/Goobo.cs
+++ RiskyMod/Items/DLC1/Equipment/Goobo.cs
@@ -17,8 +17,8 @@ namespace RiskyMod.Items.DLC1.Equipment
 
             On.RoR2.Projectile.GummyCloneProjectile.SpawnGummyClone += (orig, self) =>
             {
-                self.hpBoostCount = 70;
-                self.damageBoostCount = 35;
+                self.hpBoostCount = 70; //20 vanilla
+                self.damageBoostCount = 35; //20 vanilla
                 orig(self);
             };
         }
