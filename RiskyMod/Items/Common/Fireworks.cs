112b8351fac1ee71dbfdaa921451c7114676e1b7
Moffein <22742925+Moffein@users.noreply.github.com>
2022-05-02 22:29:20 -0700

    Fireworks no longer trigger bands.

--- RiskyMod/Items/Common/Fireworks.cs
+++ RiskyMod/Items/Common/Fireworks.cs
@@ -16,16 +16,14 @@
         {
 			if (!enabled) return;
 
-			if (Uncommon.ElementalBands.enabled)
-			{
-				projectilePrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Firework/FireworkProjectile.prefab").WaitForCompletion().InstantiateClone("RiskyMod_Fireworks", true);
-				DamageAPI.ModdedDamageTypeHolderComponent mdc = projectilePrefab.AddComponent<DamageAPI.ModdedDamageTypeHolderComponent>();
-				mdc.Add(SharedDamageTypes.DontTriggerBands);
-			}
-
+			projectilePrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Firework/FireworkProjectile.prefab").WaitForCompletion().InstantiateClone("RiskyMod_Fireworks", true);
+			DamageAPI.ModdedDamageTypeHolderComponent mdc = projectilePrefab.AddComponent<DamageAPI.ModdedDamageTypeHolderComponent>();
+			mdc.Add(SharedDamageTypes.DontTriggerBands);
+			Content.Content.projectilePrefabs.Add(projectilePrefab);
 
 			On.RoR2.FireworkLauncher.FixedUpdate += (orig, self) =>
 			{
+				self.projectilePrefab = Fireworks.projectilePrefab;	//Need to find a better place to put this
 				SceneDef sd = RoR2.SceneCatalog.GetSceneDefForCurrentScene();
 				if (sd && sd.baseSceneName.Equals("bazaar"))
 				{
