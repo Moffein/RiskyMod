52c06edb0738297a6402a769a7e80fdcda97f815
Moffein <22742925+Moffein@users.noreply.github.com>
2022-05-21 17:17:04 -0700

    Tweaked ItemOutOfBounds code.

--- RiskyMod/Tweaks/ItemOutOfBounds.cs
+++ RiskyMod/Tweaks/ItemOutOfBounds.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using RoR2;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace RiskyMod.Tweaks
 {
@@ -14,19 +15,19 @@ namespace RiskyMod.Tweaks
             On.RoR2.GenericPickupController.Start += (orig, self) =>
             {
                 orig(self);
-                self.gameObject.AddComponent<PickupTracker>();
+                if (NetworkServer.active) self.gameObject.AddComponent<PickupTracker>();
             };
 
             On.RoR2.PickupDropletController.Start += (orig, self) =>
             {
                 orig(self);
-                self.gameObject.AddComponent<PickupTracker>();
+                if (NetworkServer.active) self.gameObject.AddComponent<PickupTracker>();
             };
         }
 
         public class PickupTracker : MonoBehaviour
         {
-            public static float teleportTime = 7f;
+            public static float teleportTime = 10f;
 
             public Vector3 startingPoint;
             private float lifetime = 0f;
@@ -41,14 +42,21 @@ namespace RiskyMod.Tweaks
                 lifetime += Time.fixedDeltaTime;
                 if (lifetime > PickupTracker.teleportTime)
                 {
-                    AttemptTeleport();
+                    Rigidbody rb = base.GetComponent<Rigidbody>();
+                    if (rb)
+                    {
+                        if (rb.velocity.sqrMagnitude > 9f)
+                        {
+                            AttemptTeleport();
+                        }
+                    }
+                    Destroy(this);
                 }
             }
 
             public void AttemptTeleport()
             {
                 TeleportHelper.TeleportGameObject(base.gameObject, SneedUtils.SneedUtils.FindSafeTeleportPosition(base.gameObject, startingPoint));
-                Destroy(this);
             }
         }
     }
