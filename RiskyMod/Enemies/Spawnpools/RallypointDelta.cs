27adbcaf3e74b211567478163ca5d06658ecd3ab
Moffein <22742925+Moffein@users.noreply.github.com>
2022-07-09 23:19:25 -0700

    Disabled Rallypoint Delta changes since all the changes are in Vanilla now.

--- RiskyMod/Enemies/Spawnpools/RallypointDelta.cs
+++ RiskyMod/Enemies/Spawnpools/RallypointDelta.cs
@@ -11,17 +11,8 @@ namespace RiskyMod.Enemies.Spawnpools
         public RallypointDelta()
         {
             if (!enabled) return;
-            DirectorAPI.Helpers.RemoveExistingMonsterFromStage(DirectorAPI.Helpers.MonsterNames.Imp, DirectorAPI.Stage.RallypointDelta);
-            DirectorAPI.Helpers.RemoveExistingMonsterFromStage(DirectorAPI.Helpers.MonsterNames.Imp, DirectorAPI.Stage.RallypointDeltaSimulacrum);
 
-            DirectorAPI.Helpers.AddNewMonsterToStage(DirectorCards.Imp, false, DirectorAPI.Stage.RallypointDelta);
-            DirectorAPI.Helpers.AddNewMonsterToStage(DirectorCards.Imp, false, DirectorAPI.Stage.RallypointDeltaSimulacrum);
 
-            //TODO: FIGURE OUT HOW TO ONLY RUN THIS WHEN DLC IS ACTIVE
-            DirectorAPI.Helpers.RemoveExistingMonsterFromStage(DirectorAPI.Helpers.MonsterNames.LesserWisp, DirectorAPI.Stage.RallypointDelta);
-            DirectorAPI.Helpers.RemoveExistingMonsterFromStage(DirectorAPI.Helpers.MonsterNames.LesserWisp, DirectorAPI.Stage.RallypointDeltaSimulacrum);
-            DirectorAPI.Helpers.AddNewMonsterToStage(DirectorCards.BlindPestSnowy, false, DirectorAPI.Stage.RallypointDelta);
-            DirectorAPI.Helpers.AddNewMonsterToStage(DirectorCards.BlindPestSnowy, false, DirectorAPI.Stage.RallypointDeltaSimulacrum);
         }
     }
 }
