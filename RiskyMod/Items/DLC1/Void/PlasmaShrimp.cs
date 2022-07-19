a25447a4a7c69d2ef9202b807dc9635bb4ff3761
Moffein <22742925+Moffein@users.noreply.github.com>
2022-07-19 15:02:29 -0700

    Plasma Shrimp now scales off of proc coefficient.

--- RiskyMod/Items/DLC1/Void/PlasmaShrimp.cs
+++ RiskyMod/Items/DLC1/Void/PlasmaShrimp.cs
@@ -2,6 +2,8 @@
 using System;
 using RoR2;
 using RoR2.Orbs;
+using Mono.Cecil.Cil;
+using UnityEngine;
 
 namespace RiskyMod.Items.DLC1.Void
 {
@@ -20,29 +22,52 @@ namespace RiskyMod.Items.DLC1.Void
                 ILCursor c = new ILCursor(il);
                 if(c.TryGotoNext(
                      x => x.MatchLdsfld(typeof(DLC1Content.Items), "MissileVoid")
-                    )
-                    &&
-                     c.TryGotoNext(
-                     x => x.MatchLdcR4(0.4f)
-                    ))
+                     ))
                 {
-                    c.Next.Operand = 0.24f;
-                    c.Index += 4;
-                    c.EmitDelegate<Func<float, float>>(damage =>
+                    //Scale proc chance off of proc coefficient
+                    if (c.TryGotoNext(MoveType.After,
+                    x => x.MatchLdfld<HealthComponent>("shield")
+                    ))
                     {
-                        return damage + 0.16f;
-                    });
-                    if (RiskyMod.disableProcChains)
-                    {
-                        if (c.TryGotoNext(
-                             x => x.MatchStfld<GenericDamageOrb>("procCoefficient")
-                            ))
+                        c.Emit(OpCodes.Ldarg_1);    //damageInfo
+                        c.Emit(OpCodes.Ldloc, 4);    //master
+                        c.EmitDelegate<Func<float, DamageInfo, CharacterMaster, float>>((origShield, damageInfo, master) =>
                         {
-                            c.Index--;
-                            c.Next.Operand = 0f;
+                            if (origShield <= 0f) return origShield;//If shield is <= 0, dont bother rolling since the damage will be rejected anywyas.
+
+                            bool shouldFire = true;
+                            if (damageInfo.procCoefficient < 1f)
+                            {
+                                shouldFire = Util.CheckRoll(100f * damageInfo.procCoefficient, master);
+                            }
+
+                            return shouldFire ? origShield : 0f;    //Say that the player has 0 shield to stop the missile from firing.
+                        });
+
+                        //Modify damage
+                        if (c.TryGotoNext(
+                         x => x.MatchLdcR4(0.4f)
+                        ))
+                        {
+                            c.Next.Operand = 0.24f; //Stack damage
+                            c.Index += 4;
+                            c.EmitDelegate<Func<float, float>>(damage =>
+                            {
+                                return damage + 0.16f;  //Add to stack damage to get first stack damage
+                            });
+                            if (RiskyMod.disableProcChains)
+                            {
+                                if (c.TryGotoNext(
+                                     x => x.MatchStfld<GenericDamageOrb>("procCoefficient")
+                                    ))
+                                {
+                                    c.Index--;
+                                    c.Next.Operand = 0f;
+                                }
+                            }
+                            error = false;
                         }
                     }
-                    error = false;
                 }
 
                 if (error)
