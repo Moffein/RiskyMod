bc4f67847741c8f32bbafe0c0e92cb168e1177f3
Moffein <22742925+Moffein@users.noreply.github.com>
2022-10-15 23:09:34 -0700

    Fixed styling on Razorwire description.

--- RiskyMod/RiskyMod.cs
+++ RiskyMod/RiskyMod.cs
@@ -77,7 +77,7 @@ namespace RiskyMod
 
     [BepInDependency("com.rune580.riskofoptions")]
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.RiskyLives.RiskyMod", "RiskyMod Beta", "0.13.2")]
+    [BepInPlugin("com.RiskyLives.RiskyMod", "RiskyMod Beta", "0.13.3")]
     [R2API.Utils.R2APISubmoduleDependency(nameof(RecalculateStatsAPI), nameof(PrefabAPI), nameof(DamageAPI), nameof(SoundAPI), nameof(ItemAPI))]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
     public class RiskyMod : BaseUnityPlugin
