59287399d698b0dd473340779511edf251c005ad
Moffein <22742925+Moffein@users.noreply.github.com>
2022-12-29 17:32:45 -0800

    Converted Lunar Wisp attack to a projectile attack.

--- RiskyMod/Enemies/Mobs/Lunar/LunarWisp.cs
+++ RiskyMod/Enemies/Mobs/Lunar/LunarWisp.cs
@@ -3,7 +3,9 @@ using System;
 using RoR2;
 using RoR2.Projectile;
 using UnityEngine;
-
+using UnityEngine.AddressableAssets;
+using EntityStates.LunarWisp;
+using R2API;
 
 namespace RiskyMod.Enemies.Mobs.Lunar
 {
@@ -12,13 +14,23 @@ namespace RiskyMod.Enemies.Mobs.Lunar
         public static bool enabled = true;
         public static bool disableProjectileOnKill = true;
         public static bool enableFalloff = true;
+        public static bool removeHitscan = true;
+
+        public static GameObject shardProjectilePrefab;
 
         public LunarWisp()
         {
             if (enabled)
             {
                 EnableStatusConditions();
-                EnableBulletFalloff();
+                if (removeHitscan)
+                {
+                    RemoveHitscan();
+                }
+                else
+                {
+                    EnableBulletFalloff();
+                }
             }
             ModifyProjectile();
         }
@@ -111,6 +123,33 @@ namespace RiskyMod.Enemies.Mobs.Lunar
                     UnityEngine.Debug.LogError("RiskyMod: LunarWisp IL Hook failed");
                 }
             };
+        }
+
+        private void RemoveHitscan()
+        {
+            shardProjectilePrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Brother/LunarShardProjectile.prefab").WaitForCompletion().InstantiateClone("RiskyModLunarWispShardProjectile", true);
+            Content.Content.projectilePrefabs.Add(shardProjectilePrefab);
+
+            ProjectileImpactExplosion pie = shardProjectilePrefab.GetComponent<ProjectileImpactExplosion>();
+            if (pie) pie.blastDamageCoefficient = 1f;
+
+            ProjectileDirectionalTargetFinder pdtf = shardProjectilePrefab.GetComponent<ProjectileDirectionalTargetFinder>();
+            if (pdtf) pdtf.lookCone = 20f;  //was 60f
+            //UnityEngine.Object.Destroy(shardProjectilePrefab.GetComponent<ProjectileDirectionalTargetFinder>());
+            //UnityEngine.Object.Destroy(shardProjectilePrefab.GetComponent<ProjectileSteerTowardTarget>());
+            //UnityEngine.Object.Destroy(shardProjectilePrefab.GetComponent<ProjectileTargetComponent>());
+
+            //Completly override the vanilla version.
+            On.EntityStates.LunarWisp.FireLunarGuns.OnFireAuthority += (orig, self) =>
+            {
+                self.UpdateCrits();
+                bool isCrit = !self.critEndTime.hasPassed;
+                float damage = FireLunarGuns.baseDamagePerSecondCoefficient / self.baseBulletsPerSecond * self.damageStat * 1.5f;   //Supposed to be firing 2 bulletattacks.
+                self.StartAimMode(0.5f, false);
+                Ray aimRay = self.GetAimRay();
+                ProjectileManager.instance.FireProjectile(shardProjectilePrefab, aimRay.origin, Util.QuaternionSafeLookRotation(aimRay.direction), self.gameObject, damage, 0f, isCrit, DamageColorIndex.Default, null, 120f);
+            };
+            SneedUtils.SneedUtils.SetEntityStateField("EntityStates.LunarWisp.FireLunarGuns", "baseFireInterval", "0.125");
         }
     }
 }
