47bc2b91ee9bcd8c1be6c0b4b6b95cd7d9619fde
Moffein <22742925+Moffein@users.noreply.github.com>
2024-09-10 13:22:16 -0700

    Persistent desperado fix?

--- RiskyMod/Survivors/Bandit2/PersistentDesperado.cs
+++ RiskyMod/Survivors/Bandit2/PersistentDesperado.cs
@@ -15,6 +15,9 @@ namespace RiskyMod.Survivors.Bandit2
         {
             if (!enabled) return;
 
+            On.RoR2.CharacterBody.OnClientBuffsChanged += CharacterBody_OnClientBuffsChanged;
+            On.RoR2.CharacterBody.Start += CharacterBody_Start;
+
             IL.EntityStates.Bandit2.Weapon.FireSidearmSkullRevolver.ModifyBullet += (il) =>
             {
                 ILCursor c = new ILCursor(il);
@@ -31,6 +34,43 @@ namespace RiskyMod.Survivors.Bandit2
             };
         }
 
+        private void CharacterBody_Start(On.RoR2.CharacterBody.orig_Start orig, CharacterBody self)
+        {
+            orig(self);
+            UpdateDesperado(self);
+        }
+
+        private void CharacterBody_OnClientBuffsChanged(On.RoR2.CharacterBody.orig_OnClientBuffsChanged orig, CharacterBody self)
+        {
+            orig(self);
+            UpdateDesperado(self);
+        }
+
+        private void UpdateDesperado(CharacterBody self)
+        {
+            if (self.bodyIndex == Bandit2Core.Bandit2Index && self.master)
+            {
+                DesperadoTracker dt = self.master.GetComponent<DesperadoTracker>();
+                if (!dt) dt = self.master.gameObject.AddComponent<DesperadoTracker>();
+                if (dt)
+                {
+                    int current = self.GetBuffCount(RoR2Content.Buffs.BanditSkull);
+                    if (current > dt.count)
+                    {
+                        dt.count = current;
+                    }
+                    else
+                    {
+                        int diff = dt.count - current;
+                        for (int i = 0; i < diff; i++)
+                        {
+                            self.AddBuff(RoR2Content.Buffs.BanditSkull);
+                        }
+                    }
+                }
+            }
+        }
+
         public static float GetDesperadoMult(CharacterBody cb)
         {
             int buffCount = cb.GetBuffCount(RoR2Content.Buffs.BanditSkull.buffIndex);
@@ -43,5 +83,10 @@ namespace RiskyMod.Survivors.Bandit2
                 return 1f + buffCount * 0.1f;
             }
         }
+    }
+
+    public class DesperadoTracker : MonoBehaviour
+    {
+        public int count = 0;
     }
 }
