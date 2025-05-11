ff9f1bd6f8eaedd31b49bd1c8c2d8ad29b01fe96
Moffein <22742925+Moffein@users.noreply.github.com>
2025-05-10 21:57:39 -0500

    regigigas compat for drone resistances

--- RiskyMod/Allies/DamageResistances/SuperAttackResist.cs
+++ RiskyMod/Allies/DamageResistances/SuperAttackResist.cs
@@ -4,6 +4,7 @@ using R2API;
 using RiskyMod.Tweaks.CharacterMechanics;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using System;
 
 namespace RiskyMod.Allies
 {
@@ -15,6 +16,35 @@ namespace RiskyMod.Allies
         {
             if (!enabled) return;
             TakeDamage.ModifyInitialDamageNoAttackerActions += AddResist;
+            RoR2Application.onLoad += CheckRegigigas;
+        }
+
+        private static BodyIndex regiBody;
+        private static void CheckRegigigas()
+        {
+            //RegigigasPlayerBody, not necessary
+            regiBody = BodyCatalog.FindBodyIndex("RegigigasBody");
+            if (regiBody != BodyIndex.None) TakeDamage.ModifyInitialDamageActions += AddRegiResist;
+        }
+
+        private static void AddRegiResist(DamageInfo damageInfo, HealthComponent self, CharacterBody attackerBody)
+        {
+            if (attackerBody.bodyIndex != regiBody || attackerBody.isPlayerControlled) return;
+
+            if ((self.body.teamComponent && self.body.teamComponent.teamIndex == TeamIndex.Player)
+                && (self.body.inventory && self.body.inventory.GetItemCount(AllyItems.AllyMarkerItem) > 0))
+            {
+                if (damageInfo.damageType.damageType.HasFlag(DamageType.AOE)) damageInfo.damage *= 0.3333333333f;   //Restore old AoE resist
+
+                //Cap damage to 90% of ally's combined health
+                float effectiveArmor = self.body.armor + self.adaptiveArmorValue;
+                float realDamage = damageInfo.damage * (100f / (100f + effectiveArmor));
+                float maxDamage = (self.fullCombinedHealth) * 0.9f;
+                if (realDamage > maxDamage)
+                {
+                    damageInfo.damage *= maxDamage / realDamage;
+                }
+            }
         }
 
         private static void AddResist(DamageInfo damageInfo, HealthComponent self)
