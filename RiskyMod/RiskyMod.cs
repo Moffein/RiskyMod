0a01a777e8a5cfe64909b0dbff2ff7e00ee24a7f
Moffein <22742925+Moffein@users.noreply.github.com>
2025-05-31 04:10:11 -0500

    updated version number

--- RiskyMod/RiskyMod.cs
+++ RiskyMod/RiskyMod.cs
@@ -85,7 +85,7 @@ namespace RiskyMod
     [BepInDependency("com.Moffein.RiskyFixes")]
     [BepInDependency("com.Moffein.AssistManager")]
     [BepInDependency("com.Moffein.DefenseMatrixManager")]
-    [BepInPlugin("com.RiskyLives.RiskyMod", "RiskyMod", "2.4.6")]
+    [BepInPlugin("com.RiskyLives.RiskyMod", "RiskyMod", "2.5.0")]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
     public class RiskyMod : BaseUnityPlugin
     {
